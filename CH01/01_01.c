c6adcc20ccbdee8ae9df733241f04217bf149591
debuer <debuer.cn@gmail.com>
2022-12-10 14:45:14 +0800

    提交第一章
    源代码

---
+++ CH01/01_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int cat ;
+    printf("how much cat do you have ? \n");
+    scanf("%d",& cat);
+    printf("OK , you have %d cat \n",cat);
+    return 0 ;
+}
---
+++ CH01/01_02.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("concrete contains gravel and cement . \n");
+    return 0 ;
+}
